8c1abb16716887638b1499bd58a2ffaf42d911a4
贺春旸 <chunyang_he@139.com>
2024-08-13 10:16:55 +0800

    pt-slave-repair工具版本号: 1.0.8，更新日期：2024-08-13
    pt-slave-repair工具版本号: 1.0.8，更新日期：2024-08-13

--- src_mariadb/pt-slave-repair_mariadb.py
+++ src_mariadb/pt-slave-repair_mariadb.py
@@ -16,7 +16,7 @@ parser.add_argument('-u', '--slave_user', type=str, help='Slave User', required=
 parser.add_argument('-p', '--slave_password', type=str, help='Slave Password', required=True)
 parser.add_argument('-d', '--db_name', type=str, help='Database Name', required=True)
 parser.add_argument('-e', '--enable-binlog', dest='enable_binlog', action='store_true', default=False, help='Enable binary logging of the restore data')
-parser.add_argument('-v', '--version', action='version', version='pt-slave-repair工具版本号: 1.0.6，更新日期：2023-09-28')
+parser.add_argument('-v', '--version', action='version', version='pt-slave-repair工具版本号: 1.0.8，更新日期：2024-08-13')
 
 # 解析命令行参数
 args = parser.parse_args()
